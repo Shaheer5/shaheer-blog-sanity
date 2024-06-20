b4ba0518d1c0899a222fb642acf4c5836a2a9878
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-06-20 17:21:31 +0500

    "Added 'content' and 'launchAt' fields to 'blog' schema type and registered it in schemaTypes index"

--- schemaTypes/blog.js
+++ schemaTypes/blog.js
@@ -1,12 +1,28 @@
 export default {
   name: 'blog',
-  type: 'document', 
-    title: 'Blog',
+  type: 'document',
+  title: 'Blog',
   fields: [
     {
       name: 'title',
       type: 'string',
-      title: 'Title'
-    }
-  ]
+      title: 'Title',
+    },
+    {
+      name: 'content',
+      type: 'string',
+      title: 'Title',
+    },
+    {
+      name: 'launchAt',
+      type: 'datetime',
+      title: 'Launch Scheduled At',
+      options: {
+        dateFormat: 'YYYY-MM-DD',
+        timeFormat: 'HH:mm',
+        timeStep: 15,
+        calendarTodayLabel: 'Today',
+      },
+    },
+  ],
 }
